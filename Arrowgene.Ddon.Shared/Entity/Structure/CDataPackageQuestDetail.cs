aba75e4981e394bdb9fba92a763f9d58a9c18b16
hankhotspur <hankhotspur@proton.me>
2026-08-09 01:07:41 +0100

    Progress fields renaming

--- Arrowgene.Ddon.Shared/Entity/Structure/CDataPackageQuestDetail.cs
+++ Arrowgene.Ddon.Shared/Entity/Structure/CDataPackageQuestDetail.cs
@@ -12,8 +12,8 @@ namespace Arrowgene.Ddon.Shared.Entity.Structure
         }
 
         public uint SeqNo { get; set; }
-        public int Unk1 { get; set; }
-        public int Unk2 { get; set; }
+        public int CurrentProgress { get; set; }
+        public int MaxProgress { get; set; }
         public List<CDataCommonU32> Unk3 { get; set; }
         public bool Unk4 { get; set; }
         public bool Unk5 { get; set; }
@@ -24,8 +24,8 @@ namespace Arrowgene.Ddon.Shared.Entity.Structure
             public override void Write(IBuffer buffer, CDataPackageQuestDetail obj)
             {
                 WriteUInt32(buffer, obj.SeqNo);
-                WriteInt32(buffer, obj.Unk1);
-                WriteInt32(buffer, obj.Unk2);
+                WriteInt32(buffer, obj.CurrentProgress);
+                WriteInt32(buffer, obj.MaxProgress);
                 WriteEntityList(buffer, obj.Unk3);
                 WriteBool(buffer, obj.Unk4);
                 WriteBool(buffer, obj.Unk5);
@@ -36,8 +36,8 @@ namespace Arrowgene.Ddon.Shared.Entity.Structure
             {
                 CDataPackageQuestDetail obj = new CDataPackageQuestDetail();
                 obj.SeqNo = ReadUInt32(buffer);
-                obj.Unk1 = ReadInt32(buffer);
-                obj.Unk2 = ReadInt32(buffer);
+                obj.CurrentProgress = ReadInt32(buffer);
+                obj.MaxProgress = ReadInt32(buffer);
                 obj.Unk3 = ReadEntityList<CDataCommonU32>(buffer);
                 obj.Unk4 = ReadBool(buffer);
                 obj.Unk5 = ReadBool(buffer);
